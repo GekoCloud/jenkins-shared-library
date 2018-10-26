4f2f925fa7fddc07166cb38f304b8ffdb05e9df8
David Pujadas <dpujadas@gmail.com>
2018-10-26 11:32:35 +0200

    fix typo

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,9 +1,9 @@
 def call(String registry, String user, String pass, String image, String tag) {
-  def docker_image = "${registry}/${image}:${tag}"
+  dockerImage = "${registry}/${image}:${tag}"
 
   sh '''
-    docker image build -t ${docker_image} .
+    docker image build -t ${dockerImage} .
     docker login ${registry} -u '${user}' -p '${pass}'
-    docker image push ${docker_image}
+    docker image push ${dockerImage}
   '''
 }
