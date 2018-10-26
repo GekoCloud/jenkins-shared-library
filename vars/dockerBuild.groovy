be20fb7cf1c746b3e0e84d988d396361da737229
David Pujadas <dpujadas@gmail.com>
2018-10-26 11:37:33 +0200

    wip

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,9 +1,9 @@
 def call(String registry, String user, String pass, String image, String tag) {
   dockerImage = "${registry}/${image}:${tag}"
 
-  sh '''
+  sh """
     docker image build -t ${dockerImage} .
     docker login ${registry} -u '${user}' -p '${pass}'
     docker image push ${dockerImage}
-  '''
+  """
 }
